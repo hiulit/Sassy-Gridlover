0fceceed10831de938966f92c474084eaf32f99b
hiulit <hiulit@gmail.com>
2017-04-07 12:40:39 +0200

    Change test path to point at /example for better testing

--- test/travis.rb
+++ test/travis.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-result = `sass test/main.scss test/built.css`
+result = `sass example/scss/main.scss example/css/main.css`
 raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('test/built.css')
+raise "When compiled the module should output some CSS" unless File.exists?('example/css/built.css')
 puts "Regular compile worked successfully"
